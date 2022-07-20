f4255ff1a57b94e1653fdb779598cf45dad6b5ed
Lucastepp <lucastep.uk@gmail.com>
2022-07-20 22:38:48 +0100

    0063

---
+++ Angular14-Masterclass/TypeScript/module/employee.service.ts
@@ -0,0 +1,14 @@
+import { add, subtract, multiply, divide } from './math.util'
+
+export function generatePayslip() {
+    console.log('Generated payslip')
+}
+
+ export function calculateSalary() {
+    add(1, 2);
+    console.log('Calculated Salary!')
+}
+
+ export function log() {
+    console.log('Logged from employee service!')
+}
