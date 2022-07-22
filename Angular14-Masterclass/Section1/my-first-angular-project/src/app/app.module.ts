aafbf28406e7aa40d6e38c83ff984acb7e93230a
Lucastepp <lucastep.uk@gmail.com>
2022-07-22 22:43:07 +0100

    0067

--- Angular14-Masterclass/Section1/my-first-angular-project/src/app/app.module.ts
+++ Angular14-Masterclass/Section1/my-first-angular-project/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { TransformDirective } from './transform.directive';
 import { SafePipe } from './pipes/safe/safe.pipe';
 import { CoreModule } from './core/core.module';
 import { ChatComponent } from './components/chat/chat.component';
 import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/users/user/user.component';
 
 @NgModule({
   declarations: [
