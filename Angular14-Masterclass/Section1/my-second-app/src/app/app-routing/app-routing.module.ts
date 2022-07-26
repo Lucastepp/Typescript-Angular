a54f7a5bfd6b96ee6306a1f788a87c063f5624eb
Lucastepp <lucastep.uk@gmail.com>
2022-07-26 21:24:38 +0100

    0078

--- Angular14-Masterclass/Section1/my-second-app/src/app/app-routing/app-routing.module.ts
+++ Angular14-Masterclass/Section1/my-second-app/src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from '../components/about/about.component';
 import { BlogComponent } from '../components/blog/blog.component';
 import { HomeComponent } from '../components/home/home.component';
 
-const appRoutes: Routes = [
+
+export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'about', component: AboutComponent }
@@ -13,6 +15,7 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
+    CommonModule,
     RouterModule.forRoot(appRoutes)
   ],
   exports: [
--- Angular14-Masterclass/Section1/my-second-app/src/app/app.module.ts
+++ Angular14-Masterclass/Section1/my-second-app/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
