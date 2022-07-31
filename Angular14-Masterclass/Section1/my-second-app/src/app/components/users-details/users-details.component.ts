fb185e9e8d9033db6fb726efd6ec0b7654a08456
Lucastepp <lucastep.uk@gmail.com>
2022-07-31 20:55:11 +0100

    0081

--- Angular14-Masterclass/Section1/my-second-app/src/app/components/users-details/users-details.component.ts
+++ Angular14-Masterclass/Section1/my-second-app/src/app/components/users-details/users-details.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersDetailsComponent implements OnInit {
 
+  users: Array<{ id: number, name: string, email: string}> = [
+    { id: 1, name: 'John Doe', email: 'john@gmail.com'},
+    { id: 2, name: 'Jacob Riglin', email: 'JRiglin@gmail.com'},  
+    { id: 3, name: 'Tom Jauncey', email: 'Tom23@gmail.com'},
+    { id: 4, name: 'Jeremy Jauncey', email: 'Jeremyaa34@gmail.com'},
+    { id: 5, name: 'Sam Kodler', email: 'Kodler311@gmail.com'},
+    { id: 6, name: 'Jordan Taylor', email: 'Jo-ay@gmail.com'},
+    { id: 7, name: 'Peter Smith', email: 'Pet777@gmail.com'},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
